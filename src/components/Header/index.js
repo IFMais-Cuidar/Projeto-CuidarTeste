7b58571922ae7c7295357823bed7c847633e31f9
Bruno Ferreira <bruno.ferreira@ifmg.edu.br>
2021-12-01 09:26:40 -0300

    Testando o resolver

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -64,7 +64,7 @@ export function Header({dados}){
                             900: {
                               plugins: [
                                {
-                                 resolve: slidesToShowPlugin,
+                                 resolve: autoplayPlugin,//slidesToShowPlugin,
                                  options: {
                                   numberOfSlides: 1
                                  }
